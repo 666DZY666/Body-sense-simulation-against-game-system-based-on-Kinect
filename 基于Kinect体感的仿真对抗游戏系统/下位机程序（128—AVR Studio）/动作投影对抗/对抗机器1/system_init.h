33cf97a7b7f45a1e8240a0c77a2cc9ebea2b9ac8
666dzy666 <568314913@qq.com>
2018-08-13 23:18:53 +0800

    log message

--- 基于Kinect体感的仿真对抗游戏系统/下位机程序（128—AVR Studio）/动作投影对抗/对抗机器1/system_init.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYSTEM_INIT_H_
-#define SYSTEM_INIT_H_
-
-
-void Init_Gpio(void);
-void Init_Uart0(void);
-void System_Init(void);
-#endif /* SYSTEM_INIT_H_ */
--- 基于Kinect体感的仿真对抗游戏系统/下位机程序（128—AVR Studio）/动作投影技术挑战赛（参考）/机器2/driver.h
+++
@@ -1,7 +0,0 @@
-#ifndef DRIVER_H_
-#define DRIVER_H_
-
-void move(int l_speed,int r_speed) ;
-void PWM_Init();
-
-#endif
